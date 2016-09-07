519995d1cc60d60e6985146605ff9002b2b6bbbc
Nadav Slotky <nadav.slotky@mail.huji.ac.il>
2016-09-07 14:39:27 +0300

    small fixes

--- Measurement/CaenStop.m
+++ Measurement/CaenStop.m
@@ -1,7 +1,9 @@
 function CaenStop(~,~,gui,obj,n)
     %CAENSTOP shuts down channel n
     
-    query(obj,'$BD:0,CMD:SET,CH:',n,',PAR:VSET,VAL:0');
+    query(obj,['$BD:0,CMD:SET,CH:',n,',PAR:VSET,VAL:0']);
     delete(gui.CaenStopWindow)
+    gui.CaenStopBool = 0;
+    guidata(gui_object,gui);
     
 end
